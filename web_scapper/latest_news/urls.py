853bbae8a5a2606aedcc383e2f02c8f43c100167
Mauneel Sorathia <62179014+mauneel33@users.noreply.github.com>
2021-01-23 10:50:09 +0530

    Add files via upload
    Urls.py and views.py updated to include many templates

--- web_scapper/latest_news/urls.py
+++ web_scapper/latest_news/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from latest_news import views
 
 urlpatterns = [
-    path("", views.index, name='home')
+    path("home", views.index, name='home'),
+    path("international", views.international, name='international'),
+    path("national", views.national, name='national'),
+    path("sports", views.sports, name='sports'),
+    path("finance", views.finance, name='finance'),
+    path("entertainment", views.entertainment, name='entertainment')
 ]
