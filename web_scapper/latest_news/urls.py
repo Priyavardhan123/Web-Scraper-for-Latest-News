a59d2fc3b51a46a7a0a9ff9fe857305b60cb3514
Mauneel Sorathia <62179014+mauneel33@users.noreply.github.com>
2021-01-30 11:26:22 +0530

    User Module Added
    User Module Added - admin login

--- web_scapper/latest_news/urls.py
+++ web_scapper/latest_news/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("national", views.national, name='national'),
     path("sports", views.sports, name='sports'),
     path("finance", views.finance, name='finance'),
-    path("entertainment", views.entertainment, name='entertainment')
+    path("entertainment", views.entertainment, name='entertainment'),
+    path("adminlogin", views.adminlogin, name='adminlogin'),
+    path("login", views.login, name='login'),
+    path("reported", views.reported, name='reported'),
+    path("logout", views.logout, name='logout')
 ]
--- web_scapper/latest_news/views.py
+++ web_scapper/latest_news/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from django.template.context_processors import csrf
+from django.http import HttpResponse, HttpResponseRedirect
+from web_scapper.utils import get_MongoClient
 
 # Create your views here.
 def index(request):
@@ -17,4 +20,45 @@ def finance(request):
     return render(request, 'finance.html') 
 
 def entertainment(request):
-    return render(request, 'entertainment.html') 
+    return render(request, 'entertainment.html')
+
+def adminlogin(request):
+    if not request.session.get('useremail', None):
+        print("user is not logged in")
+        c = {}
+        c.update(csrf(request))
+        return render(request, 'adminlogin.html', c)
+    else:
+        return HttpResponseRedirect('/reported')
+    
+
+def login(request):
+    useremail = request.POST.get('useremail')
+    password = request.POST.get('password')
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["admin"]
+    userobj = mycol.find_one({"useremail":useremail})
+    # print(userobj]["password"])
+    if userobj is None:
+        return render(request, 'adminlogin.html', {'error':'Invalid Credentials!'})
+    else:
+        # print(userobj["password"])
+        if password == userobj["password"]:
+            request.session["useremail"] = useremail
+            return HttpResponseRedirect('/reported')
+        else:
+            return render(request, 'adminlogin.html', {'error':'Invalid Credential!'})
+    return render(request, 'adminlogin.html')
+
+def logout(request):
+    del request.session['useremail']
+    return HttpResponseRedirect('/adminlogin')
+
+def reported(request):
+    if not request.session.get('useremail', None):
+        print("user is not logged in")
+        c = {}
+        c.update(csrf(request))
+        return render(request, 'adminlogin.html', c)
+    else:
+        return render(request, 'reported.html')
