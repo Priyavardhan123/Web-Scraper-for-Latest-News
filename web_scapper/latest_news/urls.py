fb5c93c3a89b7e129375945ebfe431211f92cc0c
Mauneel Sorathia <62179014+mauneel33@users.noreply.github.com>
2021-05-06 01:02:33 +0530

    Final Changes
    views and urls

--- web_scapper/latest_news/urls.py
+++ web_scapper/latest_news/urls.py
@@ -4,6 +4,8 @@ from latest_news import views
 import time
 import threading
 import os
+from web_scapper import settings
+import web_scapper
 
 urlpatterns = [
     path("home", views.index, name='home'),
@@ -12,10 +14,18 @@ urlpatterns = [
     path("sports", views.sports, name='sports'),
     path("entertainment", views.entertainment, name='entertainment'),
     path("politics", views.politics, name='politics'),
+    path("shownews/<int:hrs>", views.oldnews, name='oldnews'),
     path("adminlogin", views.adminlogin, name='adminlogin'),
     path("login", views.login, name='login'),
+    path("adminhome", views.adminhome, name='adminhome'),
     path("reported", views.reported, name='reported'),
-    path("logout", views.logout, name='logout')
+    path("report", views.report, name='report'),
+    path("delete_news", views.delete_news, name='delete_news'),
+    path("logout", views.logout, name='logout'),
+    path("search", views.search, name='search'),
+    path("push_noti",views.push_noti, name='push_noti'),
+    #path("demo_notif", views.demo_notif, name="notification_demo"),
+    #path("send_notif", views.send_notif, name="send_notification")
 ]
 
 def startup():
--- web_scapper/latest_news/views.py
+++ web_scapper/latest_news/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from django.template.context_processors import csrf
 from django.http import HttpResponse, HttpResponseRedirect
-#from web_scapper.utils import get_MongoClient
+from django.views.decorators.http import require_POST
 import os
 import pymongo
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from bson import ObjectId
+import time
+import threading
 
+#Push Notification imports
+from webpush import send_user_notification
+from webpush import send_group_notification
 
+#ML Model imports
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -18,7 +26,8 @@ from sklearn.manifold import TSNE
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import os
-# Create your views here.
+
+login_error = "Invalid Credentials!"
 
 db_name = "web_scraper"
 port = 27017
@@ -27,11 +36,7 @@ username = ""
 password = ""
 myclient = None
 
-
-def get_MongoClient():
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["web-scraper"]
-    return myclient, mydb
+# Create your views here.
 
 def get_weather():
     page1 = requests.get('https://www.google.com/search?lr=lang_en&ie=UTF-8&q=weather')
@@ -65,6 +70,7 @@ def index(request):
         current_time = datetime.now()
         duration = (current_time - time).total_seconds()
         i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
         news_list.append(i)
 
         # Delete news with duration more than 36 hrs(129600 seconds) 
@@ -75,14 +81,19 @@ def index(request):
     for i in range(5):
         last_five.append(news_list.pop())
     
+    #Sending Push Notification
+    head = last_five[0]['Headline']
+    body = last_five[0]['Content']
+    send_pn(head, body)
+    
     last_eight = []
     for i in range(8):
         last_eight.append(news_list.pop())
 
     weather_report = get_weather()
             
-    return render(request, 'index.html', {'news_list' : news_list , 'last_five' : last_five, 'last_eight' : last_eight, 'weather_report' : weather_report}) 
-    
+    return render(request, 'index.html', {'news_list' : news_list , 'last_five' : last_five, 'last_eight' : last_eight, 'weather_report' : weather_report})
+
 def business(request):
     # os.system('python3 latest_news/fetch_news.py')
 
@@ -97,6 +108,7 @@ def business(request):
         current_time = datetime.now()
         duration = (current_time - time).total_seconds()
         i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
         news_list.append(i)
 
         # Delete news with duration more than 36 hrs(129600 seconds) 
@@ -121,7 +133,7 @@ def business(request):
     
     weather_report = get_weather()
 
-    return render(request, 'business.html',{'news_list' : news_list , 'last_three' : last_three, 'last_six': last_six, 'weather_report' : weather_report}) 
+    return render(request, 'business.html',{'news_list' : news_list , 'last_three' : last_three, 'last_six': last_six, 'weather_report' : weather_report})
 
 def tech(request):
     # os.system('python3 latest_news/fetch_news.py')
@@ -137,6 +149,7 @@ def tech(request):
         current_time = datetime.now()
         duration = (current_time - time).total_seconds()
         i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
         news_list.append(i)
 
         # Delete news with duration more than 36 hrs(129600 seconds) 
@@ -177,6 +190,7 @@ def sports(request):
         current_time = datetime.now()
         duration = (current_time - time).total_seconds()
         i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
         news_list.append(i)
 
         # Delete news with duration more than 36 hrs(129600 seconds) 
@@ -217,6 +231,7 @@ def entertainment(request):
         current_time = datetime.now()
         duration = (current_time - time).total_seconds()
         i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
         news_list.append(i)
 
         # Delete news with duration more than 36 hrs(129600 seconds) 
@@ -257,6 +272,7 @@ def politics(request):
         current_time = datetime.now()
         duration = (current_time - time).total_seconds()
         i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
         news_list.append(i)
 
         # Delete news with duration more than 36 hrs(129600 seconds) 
@@ -281,7 +297,7 @@ def politics(request):
     
     weather_report = get_weather()
 
-    return render(request, 'politics.html', {'news_list' : news_list , 'last_three' : last_three, 'last_six': last_six, 'weather_report' : weather_report}) 
+    return render(request, 'politics.html', {'news_list' : news_list , 'last_three' : last_three, 'last_six': last_six, 'weather_report' : weather_report})
 
 def adminlogin(request):
     if not request.session.get('useremail', None):
@@ -290,9 +306,9 @@ def adminlogin(request):
         c.update(csrf(request))
         return render(request, 'adminlogin.html', c)
     else:
-        return HttpResponseRedirect('/reported')
-
-
+        return HttpResponseRedirect('/adminhome')
+    
+@require_POST
 def login(request):
     useremail = request.POST.get('useremail')
     password = request.POST.get('password')
@@ -301,29 +317,221 @@ def login(request):
     userobj = mycol.find_one({"useremail":useremail})
     # print(userobj]["password"])
     if userobj is None:
-        return render(request, 'adminlogin.html', {'error':'Invalid Credentials!'})
+        return render(request, 'adminlogin.html', {'error':login_error})
     else:
         # print(userobj["password"])
         if password == userobj["password"]:
             request.session["useremail"] = useremail
-            return HttpResponseRedirect('/reported')
+            return HttpResponseRedirect('/adminhome')
         else:
-            return render(request, 'adminlogin.html', {'error':'Invalid Credential!'})
+            return render(request, 'adminlogin.html', {'error':login_error})
     return render(request, 'adminlogin.html')
 
 def logout(request):
     del request.session['useremail']
     return HttpResponseRedirect('/adminlogin')
 
+# All news page for Admin
+def adminhome(request):
+    if not request.session.get('useremail', None):
+        print("user is not logged in")
+        c = {}
+        c.update(csrf(request))
+        return render(request, 'adminlogin.html', c)
+
+    c = {}
+    c.update(csrf(request))
+    #Getting news list from database
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["news_table"]
+
+    news = mycol.find()
+    news_list = []
+    for i in news:
+        time = i['DateTime']
+        current_time = datetime.now()
+        #Setting the duration
+        duration = (current_time - time).total_seconds()
+        i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
+        news_list.append(i)
+    
+    page = request.GET.get('page', 1)
+
+    #Django Pagination
+    paginator = Paginator(news_list, 10)
+
+    try:
+        news = paginator.page(page)
+    
+    #For first page
+    except PageNotAnInteger:
+        news = paginator.page(1)
+
+    #For last page
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+
+    return render(request, 'adminhome.html', {"news" : news})
+
+# Reported News page for admin
 def reported(request):
     if not request.session.get('useremail', None):
         print("user is not logged in")
         c = {}
         c.update(csrf(request))
         return render(request, 'adminlogin.html', c)
-    else:
-        return render(request, 'reported.html') 
 
+    #Getting news list from database
+    c = {}
+    c.update(csrf(request))
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["news_table"]
+
+    news = mycol.find({'Reported' : True})
+    news_list = []
+
+    for i in news:
+        time = i['DateTime']
+        current_time = datetime.now()
+        #Setting the duration
+        duration = (current_time - time).total_seconds()
+        i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
+        news_list.append(i)
+
+    nl_len = len(news_list)
+
+    page = request.GET.get('page', 1)
+
+    #Django Pagination
+    paginator = Paginator(news_list, 10)
+
+    try:
+        news = paginator.page(page)
+    
+    #For first page
+    except PageNotAnInteger:
+        news = paginator.page(1)
+
+    #For last page
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    return render(request, 'reported.html', {"news" : news, "nl_len" : nl_len})
+
+# Showing news of past x hrs
+def oldnews(request, hrs):
+
+    #Getting news list from database
+    c = {}
+    c.update(csrf(request))
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["news_table"]
+
+    news = mycol.find()
+    news_list = []
+
+    for i in news:
+        time = i['DateTime']
+        current_time = datetime.now()
+        #Setting the duration
+        duration = (current_time - time).total_seconds()
+        i['DateTime'] = int(duration//60)
+        i['id'] = i['_id']
+
+        # Keeping news which were latest by 6 hrs (21600 sec)
+        if(duration < (hrs * 3600)):
+            news_list.append(i)
+
+    nl_len = len(news_list)
+
+    page = request.GET.get('page', 1)
+
+    #Django Pagination
+    paginator = Paginator(news_list, 10)
+
+    try:
+        news = paginator.page(page)
+    
+    #For first page
+    except PageNotAnInteger:
+        news = paginator.page(1)
+
+    #For last page
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    weather_report = get_weather()
+    return render(request, 'shownews.html', {"news" : news, "nl_len" : nl_len, 'weather_report' : weather_report})
+
+@require_POST
+def delete_news(request):
+    if not request.session.get('useremail', None):
+        print("user is not logged in")
+        c = {}
+        c.update(csrf(request))
+        return render(request, 'adminlogin.html', c)
+
+    #Deleting news from database
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["news_table"]
+    id = request.POST.get('newsid')
+    mycol.delete_one({'_id' : ObjectId(id)})
+    print("\n-------News Id : ",id, "deleted successfully!--------\n")
+
+    return HttpResponseRedirect('/adminhome')
+
+@require_POST
+def report(request):
+    # Updating 'reported' field of news in database
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["news_table"]
+    id = request.POST.get('newsid')
+    myquery = { "_id": ObjectId(id)}
+    newvalues = { "$set": { "Reported": True } }
+    mycol.update_one(myquery, newvalues)
+    print("\n-------News Id : ",id, "reported successfully!--------\n")
+
+    return HttpResponseRedirect("/home")
+
+# Searching News
+@require_POST
+def search(request):
+
+    # Getting Search query
+    query = request.POST.get('squery')
+    # For null query
+    if query is None:
+        return HttpResponseRedirect("/home")
+
+    myclient, mydb = get_MongoClient()
+    mycol = mydb["news_table"]
+
+    # Using MongoDB's Text index for searching on Headline, Content, Category and Source fields
+    news = mycol.find({"$text" : {"$search" : query}})
+    news_list = []
+
+    for i in news:
+        news_list.append(i)
+    nl_len = len(news_list)
+    weather_report = get_weather()
+    return render(request, 'search.html', {"news" : news_list, "nl_len" : nl_len, 'weather_report' : weather_report})
+    
+def push_noti(request):
+    webpush = {"group": "all"}
+    return render(request, 'notification.html', {"webpush" : webpush})
+
+def demo_notif(request):
+    return render(request,'demoNotif.html')
+
+@require_POST
+def send_notif(request):
+    head = request.POST.get('head')
+    body = request.POST.get('body')
+    send_pn(head, body)
+    return HttpResponseRedirect('/demo_notif')
+
+
+#ML Algorithm for finding category of news
 def category(texts):
     df = pd.read_csv("latest_news/BBC News Train.csv")
     df['category_id'] = df['Category'].factorize()[0]
@@ -389,3 +597,19 @@ def category(texts):
         prediction_list.append((id_to_category[predicted]))
 
     return prediction_list
+
+#Getting mongoDb access
+def get_MongoClient():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["web_scraper"]
+    return myclient, mydb
+
+#Sending push notification
+def send_pn(head, body):
+    payload = {"head": head, "body": body, "icon": "/static/images/notification.png", "url": "http://127.0.0.1:8000/home"}
+    try:
+        send_group_notification(group_name="all", payload=payload, ttl=1000)
+    except Exception as e:
+        print(e)
+
+
