d20bd52244e8e4b82111d6dde41fff525e53b2d3
Mauneel Sorathia <62179014+mauneel33@users.noreply.github.com>
2021-05-06 01:04:43 +0530

    Final Changes
    Settings and urls

--- web_scapper/web_scapper/urls.py
+++ web_scapper/web_scapper/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('latest_news.urls'))
+    path('',include('latest_news.urls')),
+    path('webpush/', include('webpush.urls'))
 ]
